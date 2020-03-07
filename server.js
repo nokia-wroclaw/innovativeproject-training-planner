cea72bde16bab9b05cfbb1ade40b46fabc099fb8
Cvaniak <igna.cwaniak@gmail.com>
2020-03-07 01:08:03 +0100

    Heroku and Travis Update

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const app = express();  
+const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.PROD_MONGODB || 'mongodb://localhost/test' ;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
