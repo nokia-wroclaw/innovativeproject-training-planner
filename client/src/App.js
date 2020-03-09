aa020b41577558016e50fac53d08e8d44d91a006
jaolejnik <olejnik.jakub71@gmail.com>
2020-03-09 12:53:28 +0100

    Created invite template in JSX using antdesign

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar.component"
+import Navbar from "./components/navbar.component";
 import CreateUser from "./components/create-user.component";
+import CreateInviteTemplate from "./components/createInviteTemplate.component";
 
 function App() {
   return (
     <Router>
       <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/user" component={CreateUser} />
+        <Navbar />
+        <br />
+        <Route path="/user" component={CreateUser} />
+        <Route path="/inviteTemplate" component={CreateInviteTemplate} />
       </div>
     </Router>
   );
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path')
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,26 +11,27 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.PROD_MONGODB || 'mongodb://localhost/test' ;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+const uri = process.env.PROD_MONGODB || "mongodb://localhost/test";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
+const invTemplRouter = require("./routes/inviteTemplate");
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
+app.use("/inviteTemplate", invTemplRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
