f58c19ec295c644c6333bd265705eb2baee91894
jaolejnik <olejnik.jakub71@gmail.com>
2020-03-09 12:53:28 +0100

    Created invite template in JSX using antdesign

--- client/src/components/createInviteTemplate.component.js
+++ client/src/components/createInviteTemplate.component.js
@@ -17,7 +17,6 @@ const layout = {
 const validateMessages = {
   required: "This field is required!",
   types: {
-    email: "Not a validate email!",
     number: "Not a validate number!"
   },
   number: {
@@ -41,33 +40,58 @@ export default function CreateInviteTemplate() {
   // will create custom form ASAP
   return (
     <Form {...layout} name="nest-messages" validateMessages={validateMessages}>
+      <Form.Item name={["date"]} label="Date" rules={[{ required: true }]}>
+        <DatePicker /> <TimePicker format="HH:mm" />
+      </Form.Item>
+
       <Form.Item
-        name={["user", "name"]}
-        label="Name"
+        name={["duration"]}
+        label="Duration:"
+        rules={[{ type: "number", min: 1, max: 24, required: true }]}
+      >
+        <InputNumber /> hours
+      </Form.Item>
+
+      <Form.Item
+        name={["instructor"]}
+        label="Instructor"
         rules={[{ required: true }]}
       >
         <Input />
       </Form.Item>
-      <Form.Item
-        name={["user", "email"]}
-        label="Email"
-        rules={[{ type: "email" }]}
-      >
+
+      <Form.Item name={["title"]} label="Title:" rules={[{ required: true }]}>
         <Input />
       </Form.Item>
-      <Form.Item
-        name={["user", "age"]}
-        label="Age"
-        rules={[{ type: "number", min: 0, max: 99 }]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={["user", "website"]} label="Website">
-        <Input />
-      </Form.Item>
-      <Form.Item name={["user", "introduction"]} label="Introduction">
+
+      <Form.Item name={["agenda"]} label="Agenda:" rules={[{ required: true }]}>
         <Input.TextArea />
       </Form.Item>
+
+      <Form.Item
+        name={["description"]}
+        label="Description:"
+        rules={[{ required: true }]}
+      >
+        <Input.TextArea />
+      </Form.Item>
+
+      <Form.Item
+        name={["willLearn"]}
+        label="What you will learn:"
+        rules={[{ required: true }]}
+      >
+        <Input.TextArea />
+      </Form.Item>
+
+      <Form.Item name={["mustKnow"]} label="What you must already know:">
+        <Input.TextArea />
+      </Form.Item>
+
+      <Form.Item name={["materials"]} label="Materials:">
+        <Input.TextArea />
+      </Form.Item>
+
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
         <Button type="primary" htmlType="submit">
           Submit
