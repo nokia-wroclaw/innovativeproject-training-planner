fa72380170b158cd21d5179d64d263749a979481
Marcin Komorek <m.komorek98@gmail.com>
2020-03-18 16:50:57 +0100

    Redesign with materialize css - FRONT END

--- client/src/components/template/sendInvite.component.js
+++ client/src/components/template/sendInvite.component.js
@@ -4,7 +4,33 @@ const SendInvite = () => {
 
     return(
         <div className="container">
-            <h5 className="center">There will be possibility to send invite from selected tempalte.</h5>
+            <form className="white col s12" style={{padding:"0 20px"}}>
+                <div className="row center">
+                    <div className="input-field col s6 offset-s3">
+                        <label htmlFor="recipents">Recipents</label>
+                        <input 
+                            type="text" 
+                            id="recipents" 
+                        />
+                    </div>
+                    <div className="input-field col s6 offset-s3">
+                        <label htmlFor="subject">Subject</label>
+                        <input 
+                            type="text" 
+                            id="subject" 
+                        />
+                    </div>
+                    <div className="input-field col s6 offset-s3" style={{border: true}}>
+                        <label htmlFor="textarea1">Message</label>
+                        <textarea id="textarea1" className="materialize-textarea"></textarea>
+                    </div>
+                    <div className="input-field col s6 offset-s3">
+                        <button className="btn pink lighten-1 z-depth-0">
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
