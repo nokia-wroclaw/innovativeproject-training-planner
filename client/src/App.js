3e51870a424f09e5a3f34f8134c4d16ff6137c8a
Marcin Komorek <m.komorek98@gmail.com>
2020-03-18 10:36:26 +0100

    send invite page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from "./components/layout/navbar.component";
-import TemplateDashboard from "./components/template/templateDashboard.component"
+import TemplateDashboard from "./components/template/templateDashboard.component";
 import CreateInviteTemplate from "./components/template/createInviteTemplate.component";
+import SendInvite from "./components/template/sendInvite.component"
 import CreateUser from "./components/user/createUser.component";
 import UserProfile from "./components/user/userProfile.component";
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/useres" component={CreateUser} />
           <Route path="/templateDashboard" component={TemplateDashboard} />
           <Route path="/inviteTemplate" component={CreateInviteTemplate} />
+          <Route path="/sendInvite" component={SendInvite} />
           <Route path="/profile" component={UserProfile} />
         </Switch>
       </div>
---
+++ client/src/components/template/sendInvite.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SendInvite = () => {
+
+    return(
+        <div className="container">
+            <h5 className="center">There will be possibility to send invite from selected tempalte.</h5>
+        </div>
+    )
+}
+
+export default SendInvite;
