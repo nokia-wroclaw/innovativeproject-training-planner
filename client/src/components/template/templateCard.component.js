44c0bea58306eedb50f2ddae59a3af9d2e01d5a8
Cvaniak <igna.cwaniak@gmail.com>
2020-03-22 03:12:14 +0100

    Working list of tamplates

--- client/src/components/template/templateCard.component.js
+++ client/src/components/template/templateCard.component.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const TemplateCard = () => {
+String.prototype.trunc =
+  String.prototype.trunc ||
+  function(n) {
+    return this.length > n ? this.substr(0, n - 1) + "&hellip;" : this;
+  };
 
-    return(
-      <div className="card blue darken-1">
-        <div className="card-content white-text">
-          <span className="card-title">Training Title</span>
-          <p>Training description.Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. 
-          Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper
-          justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies 
-          purus, sed posuere libero dui id orci.</p>
-          <p>Training instructor. Date and time when start.</p>
-        </div>
-        <div className="card-action">
-          <Link to="/sendInvite">Send Invitation</Link>
-        </div>
+const TemplateCard = props => {
+  return (
+    <div className="card blue darken-1">
+
+      <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4">
+          {props.item.instructor}
+          <i className="material-icons right">close</i>
+        </span>
+        <p>{props.item.date}</p>
       </div>
-    )
-}
+
+      <div className="card-content white-text">
+        <span className="card-title activator">{props.item.title}</span>
+        <p>{props.item.description.trunc(80)}</p>
+      </div>{" "}
+
+      <div className="card-action">
+        <Link to="/sendInvite">Send Invitation</Link>
+      </div>
+
+    </div>
+  );
+};
 
 export default TemplateCard;
