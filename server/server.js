acf8650316e1e6e8d581b4d43223dfe450419448
Aleksander Pucher <pucher.aleksander@gmail.com>
2020-03-23 20:20:18 +0100

    Fixed paths and dependencies (after rebase care)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path')
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.PROD_MONGODB || 'mongodb+srv://MiJam:TraficJam@cluster0-kwmon.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const uri = process.env.PROD_MONGODB || "mongodb+srv://MiJam:TraficJam@cluster0-kwmon.gcp.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.set('useCreateIndex', true)
 
 mongoose
@@ -24,19 +24,21 @@ useNewUrlParser: true,
 console.log('DB Connection Error: ' + err);
 });
 
-const usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
 const invTemplRouter = require("./routes/inviteTemplate");
+const invRouter = require("./routes/invitation");
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 app.use("/inviteTemplate", invTemplRouter);
+app.use("/sendInvite", invRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(express.static(path.join(__dirname, "/../client")));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/index.html"));
+});
