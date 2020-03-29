fc05f0ce2d70ef306688e44ee76d38d3518537a2
Kuba <olejnik.jakub71@gmail.com>
2020-03-30 00:34:00 +0200

    Added icons to represent a type of training

--- client/src/components/template/templateCard.component.js
+++ client/src/components/template/templateCard.component.js
@@ -11,9 +11,35 @@ const sendInv = invID => {
   return `/sendInvite/${invID}`;
 };
 
+const setCardIcon = trainingType => {
+  let cardColor;
+  if (trainingType === "Software Training")
+    return <i className="material-icons right">computer</i>;
+  else if (trainingType === "Hardware Training")
+    return <i className="material-icons right">build</i>;
+  else if (trainingType === "Soft Skills Training")
+    return <i className="material-icons right">group</i>;
+  else return <i className="material-icons right">work</i>;
+};
+
+const setCardColor = trainingType => {
+  let cardColor;
+  if (trainingType === "Software Training") cardColor = "#1e88e5";
+  // blue darken-1
+  else if (trainingType === "Hardware Training") cardColor = "#7c4dff";
+  // deep-purple accent-2
+  else if (trainingType === "Soft Skills Training") cardColor = "#26a69a";
+  // teal lighten-1
+  else cardColor = "#9e9e9e";
+  return cardColor;
+};
+
 const TemplateCard = props => {
   return (
-    <div className="card hoverable blue darken-1">
+    <div
+      className="card hoverable"
+      style={{ backgroundColor: setCardColor(props.item.trainingType) }}
+    >
       <div className="card-reveal">
         <span className="card-title grey-text text-darken-4">
           {props.item.instructor}
@@ -22,11 +48,12 @@ const TemplateCard = props => {
         <p>{props.item.date}</p>
       </div>
       <div className="card-content white-text">
+        {setCardIcon(props.item.trainingType)}
         <span className="card-title activator">{props.item.title}</span>
         <p>{props.item.description.trunc(80)}</p>
       </div>{" "}
-      <div className="card-action center">
-        <Link to={sendInv(props.item._id)}>SEND INVITATION</Link>
+      <div className="card-action">
+        <Link to={sendInv(props.item._id)}>Send Invitation</Link>
       </div>
     </div>
   );
