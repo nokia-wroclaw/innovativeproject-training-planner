64bbcf20e018d3d8d6ed74c66ba4b0f42a2a18e0
jaolejnik <olejnik.jakub71@gmail.com>
2020-04-28 21:10:27 +0200

    Temporary fix: navbar margin

--- client/src/components/layout/navbar.component.js
+++ client/src/components/layout/navbar.component.js
@@ -10,26 +10,31 @@ const Navbar = () => {
   });
 
   return (
-    <nav
-      className="nav-wrapper blue darken-3 pinned"
-      style={{ position: "fixed", zIndex: 9999 }}
-      data-targe="app"
-    >
-      <Link to="/" className="brand-logo center">
-        Training Planner<i className="material-icons right">watch</i>
-      </Link>
-      <ul className="left">
-        <li>
-          <Link to="/templateDashboard">Templates</Link>
-        </li>
-      </ul>
-      <ul className="right">
-        <li>
-          {" "}
-          <UserDropDownMenu />{" "}
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav
+        className="nav-wrapper blue darken-3 pinned"
+        style={{ position: "fixed", zIndex: 9999 }}
+        data-targe="app"
+      >
+        <Link to="/" className="brand-logo center">
+          Training Planner<i className="material-icons right">watch</i>
+        </Link>
+        <ul className="left">
+          <li>
+            <Link to="/templateDashboard">Templates</Link>
+          </li>
+        </ul>
+        <ul className="right">
+          <li>
+            {" "}
+            <UserDropDownMenu />{" "}
+          </li>
+        </ul>
+      </nav>
+      <br />
+      <br />
+      <br />
+    </div>
   );
 };
 
