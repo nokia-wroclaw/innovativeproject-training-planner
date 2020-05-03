1283ff6b65648b8b4da9c99e1b3ea2e10e7c112c
jaolejnik <olejnik.jakub71@gmail.com>
2020-05-03 21:46:14 +0200

    Moved okta's code to the new file.

---
+++ server/toolset/okta.js
@@ -0,0 +1,33 @@
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+module.exports = {
+  // Function from example of Okta, used to authenticate
+  authenticationRequired: function(req, res, next) {
+    const oktaJwtVerifier = new OktaJwtVerifier({
+      issuer: `${process.env.OKTA_ISSUER}`,
+      clientId: `${process.env.OKTA_CLIENT_ID}`,
+      assertClaims: {
+        aud: 'api://default',
+      },
+    });
+    const authHeader = req.headers.authorization || '';
+    const match = authHeader.match(/Bearer (.+)/);
+
+    if (!match) {
+      return res.status(401).end();
+    }
+
+    const accessToken = match[1];
+    const expectedAudience = 'api://default';
+
+    return oktaJwtVerifier
+        .verifyAccessToken(accessToken, expectedAudience)
+        .then((jwt) => {
+          req.jwt = jwt;
+          next();
+        })
+        .catch((err) => {
+          res.status(401).send(err.message);
+        });
+  },
+};
