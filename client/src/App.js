c3593a3ad84ef227973740a0028911065aaff8b9
jaolejnik <olejnik.jakub71@gmail.com>
2020-05-04 00:39:10 +0200

    Created a new AppWithAuth component for better authentication handling. Now when user is logged out he can't access the app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-import {Security, LoginCallback} from '@okta/okta-react';
+import {BrowserRouter} from 'react-router-dom';
+import {Security} from '@okta/okta-react';
 
-import Home from './components/views/home.component';
-import Navbar from './components/layout/navbar.component';
-import SendInvite from './components/invitation/sendInvite.component';
-import UserProfile from './components/user/userProfile.component';
-import TemplateDashboard from './components/views/templateDashboard.component';
-import CreateInviteTemplate
-  from './components/template/createInviteTemplate.component';
-import GeneralCalendarDashboard
-  from './components/views/generalCalendarDashboard.compoennt';
-import UserCalendarDashboard
-  from './components/views/userCalendarDashboard.component';
+import AppWithAuth from './components/appWithAuth.component';
 
-// TODO make it a local variable or something
 const config = {
   issuer: 'https://dev-820510.okta.com/oauth2/default',
   redirectUri: window.location.origin + '/implicit/callback',
@@ -22,20 +11,11 @@ const config = {
   pkce: true,
 };
 
-const App = (props) => {
+const App = () => {
   return (
     <BrowserRouter>
       <Security {...config}>
-        <Navbar />
-        <Route exact path="/" />
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/sendInvite" component={SendInvite} />
-        <Route path="/inviteTemplate" component={CreateInviteTemplate} />
-        <Route path="/templateDashboard" component={TemplateDashboard} />
-        <Route path="/implicit/callback" component={LoginCallback} />
-        <Route path="/profile" component={UserProfile} />
-        <Route path="/userCalendar" component={UserCalendarDashboard} />
-        <Route path="/generalCalendar" component={GeneralCalendarDashboard} />
+        <AppWithAuth />
       </Security>
     </BrowserRouter>
   );
