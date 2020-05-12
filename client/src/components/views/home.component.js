61277e675f0c2ad8e9497fcddd03f19e69f05aa5
jaolejnik <olejnik.jakub71@gmail.com>
2020-05-12 23:18:23 +0200

    Added "hello {username}" to the homepage.

--- client/src/components/views/home.component.js
+++ client/src/components/views/home.component.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useOktaAuth} from '@okta/okta-react';
 import GeneralCalendar from '../calendar/generalCalendar.component';
 
 const Home = () => {
+  const {authService} = useOktaAuth();
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    authService.getUser().then((info) => {
+      setUsername(info.preferred_username);
+    });
+  }, [authService]);
+
   return (
     <div className="background">
       <div className="container center calendar-board">
+        <div className="row">
+          <h4>Hello {username}!</h4>
+        </div>
         <div className="row">
           <GeneralCalendar height="550pt" />
         </div>
